7bfdeccbc9a72bb9e7fb7797ed42b6f741472b56
tmp64 <tmp32x2@gmail.com>
2022-08-24 11:48:12 +0700

    ImGui: Create texture sampler

--- ChairLoader/ChairLoaderImGui.cpp
+++ ChairLoader/ChairLoaderImGui.cpp
@@ -436,6 +436,21 @@ bool ChairLoaderImGui::RT_Initialize() {
 			desc.MiscFlags = 0;
 			data.pd3dDevice->CreateBuffer(&desc, NULL, &data.pVertexConstantBuffer);
 		}
+
+		// Create texture sampler
+		{
+			D3D11_SAMPLER_DESC desc;
+			ZeroMemory(&desc, sizeof(desc));
+			desc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+			desc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
+			desc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+			desc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+			desc.MipLODBias = 0.f;
+			desc.ComparisonFunc = D3D11_COMPARISON_ALWAYS;
+			desc.MinLOD = 0.f;
+			desc.MaxLOD = 0.f;
+			data.pd3dDevice->CreateSamplerState(&desc, &data.pFontSampler);
+		}
 	}
 
 	// Create the pixel shader
