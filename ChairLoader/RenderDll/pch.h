8f7b076824f77a6711b7a618c95c0bb7d34a2ee7
tmp64 <tmp32x2@gmail.com>
2022-09-15 13:38:00 +0700

    RenderDll: Add PCH

---
+++ ChairLoader/RenderDll/pch.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <ChairLoader/pch.h>
+#include <Prey/RenderDll/Common/CommonRender.h>
+#include <Prey/CryRenderer/CryDX.h>
+#include <Prey/CryRenderer/IRenderer.h>
+#include <Prey/CryRenderer/IShader.h>
+#include <Prey/CryRenderer/ITexture.h>
+#include <Prey/CryRenderer/Tarray.h>
+
+#include <Prey/RenderDll/XRenderD3D9/DriverD3D.h>
