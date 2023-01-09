4558f1eacfac91b56d43b19009192d004776be69
thelivingdiamond <thelivingdiamond6@gmail.com>
2023-01-08 23:52:10 -0800

    Update Localization Utility
    Index checking + persistent options w/ chairvars

--- ChairLoader/Tools/LocalizationUtil.cpp
+++ ChairLoader/Tools/LocalizationUtil.cpp
@@ -7,22 +7,7 @@
 #include "LocalizationUtil.h"
 
 LocalizationUtil::LocalizationUtil() {
-    auto locman = (CLocalizedStringsManager*)gEnv->pSystem->GetLocalizationManager();
-    // Set all localizations to be available
-    //TODO: check for available languages first
-    locman->m_availableLocalizations |=
-            ( (1 << eLID_English)
-              | (1 << eLID_French)
-              | (1 << eLID_Italian)
-              | (1 << eLID_German)
-              | (1 << eLID_Spanish)
-              | (1 << eLID_Latin_American_Spanish)
-              | (1 << eLID_Brazilian_Portuguese)
-              | (1 << eLID_Russian)
-              | (1 << eLID_Polish)
-              | (1 << eLID_Japanese)
-              | (1 << eLID_Traditional_Chinese)
-              | (1 << eLID_Simplified_Chinese));
+    InitGame();
 }
 
 void LocalizationUtil::drawMenu() {
@@ -46,25 +31,57 @@ void LocalizationUtil::draw() {
         auto locman = (CLocalizedStringsManager *) gEnv->pSystem->GetLocalizationManager();
         // create an ImGui combo box to set the language
         if (ImGui::Begin("Localization Utility", &m_bDraw, ImGuiWindowFlags_AlwaysAutoResize)) {
+            ImGui::Text("This is for in-game localization only.\nWhile we would love to offer multiple languages for chairloader, we don't have the people needed to translate it.");
             ImGui::Text("Current Language: %s", gEnv->pConsole->GetCVar("g_language")->GetString());
             ImGui::Text("Current Audio Language: %s", gEnv->pConsole->GetCVar("g_languageAudio")->GetString());
-            const char *items[] = {"English", "French", "Italian", "German", "Spanish", "Latin American Spanish", "Brazilian Portuguese", "Russian", "Polish", "Japanese", "Traditional Chinese", "Simplified Chinese"};
-            const char *items_internal[] = {"english", "french", "italian", "german", "spanish", "latin_american_spanish", "brazilian_portuguese", "russian", "polish", "japanese", "chineset", "chineses"};
-            static int current_text_item = 0;
-            if (ImGui::Combo("Language", &current_text_item, items, IM_ARRAYSIZE(items))) {
-                const char* language = locman->LanguageNameFromID((ELanguageID) current_text_item);
-                gEnv->pConsole->GetCVar("g_language")->Set(items_internal[current_text_item]);
+            // check that the index is within bounds
+            if(m_CurrentTextItem < 0 || m_CurrentTextItem >= items.size()) {
+                m_CurrentTextItem = 0;
+                CryError("Invalid language index");
             }
-            static int current_audio_item = 0;
+            if (ImGui::Combo("Language", &m_CurrentTextItem, items.data(), items.size())) {
+                const char* language = locman->LanguageNameFromID((ELanguageID) m_CurrentTextItem);
+                gEnv->pConsole->GetCVar("g_language")->Set(items_internal[m_CurrentTextItem]);
+            }
+            // check that the index is within bounds
+            if(m_CurrentAudioItem < 0 || m_CurrentAudioItem >= items.size()) {
+                m_CurrentAudioItem = 0;
+                CryError("Invalid audio language index");
+            }
             // yellow text
             ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0f, 0.9f, 0.3f, 1.0f));
             ImGui::Text("Changing the audio language while in game may have unintended consequences. \nIt is recommended to set the audio language before loading into a level.");
             ImGui::PopStyleColor();
-            if (ImGui::Combo("Audio Language", &current_audio_item, items, IM_ARRAYSIZE(items))) {
-                const char* language = locman->LanguageNameFromID((ELanguageID) current_audio_item);
-                gEnv->pConsole->GetCVar("g_languageAudio")->Set(items_internal[current_audio_item]);
+            if (ImGui::Combo("Audio Language", &m_CurrentAudioItem, items.data(), items.size())) {
+                const char* language = locman->LanguageNameFromID((ELanguageID) m_CurrentAudioItem);
+                gEnv->pConsole->GetCVar("g_languageAudio")->Set(items_internal[m_CurrentAudioItem]);
             }
         }
         ImGui::End();
     }
+}
+
+void LocalizationUtil::InitGame() {
+    auto locman = (CLocalizedStringsManager*)gEnv->pSystem->GetLocalizationManager();
+    // Set all localizations to be available
+    //TODO: check for available languages first
+    locman->m_availableLocalizations |=
+            ( (1 << eLID_English)
+              | (1 << eLID_French)
+              | (1 << eLID_Italian)
+              | (1 << eLID_German)
+              | (1 << eLID_Spanish)
+              | (1 << eLID_Latin_American_Spanish)
+              | (1 << eLID_Brazilian_Portuguese)
+              | (1 << eLID_Russian)
+              | (1 << eLID_Polish)
+              | (1 << eLID_Japanese)
+              | (1 << eLID_Traditional_Chinese)
+              | (1 << eLID_Simplified_Chinese));
+
+    REGISTER_CVAR2("cl_textLanguage", &m_CurrentTextItem, 0, VF_DUMPTOCHAIR, "Storing the text language for the localization utility, do not change this value!");
+    REGISTER_CVAR2("cl_audioLanguage", &m_CurrentAudioItem, 0, VF_DUMPTOCHAIR, "Storing the audio language for the localization utility, do not change this value!");
+    // set the language
+    gEnv->pConsole->GetCVar("g_language")->Set(items_internal[m_CurrentTextItem]);
+    gEnv->pConsole->GetCVar("g_languageAudio")->Set(items_internal[m_CurrentAudioItem]);
 }
--- ChairLoader/Tools/LocalizationUtil.h
+++ ChairLoader/Tools/LocalizationUtil.h
@@ -10,9 +10,16 @@ class LocalizationUtil {
 public:
     LocalizationUtil();
 
+
+    void InitGame();
     void drawMenu();
     void draw();
     bool m_bDraw = false;
+private:
+    int m_CurrentTextItem = 0;
+    int m_CurrentAudioItem = 0;
+    const std::vector<const char*> items = {"English", "French", "Italian", "German", "Spanish", "Latin American Spanish", "Brazilian Portuguese", "Russian", "Polish", "Japanese", "Traditional Chinese", "Simplified Chinese"};
+    const std::vector<const char*> items_internal = {"english", "french", "italian", "german", "spanish", "latin_american_spanish", "brazilian_portuguese", "russian", "polish", "japanese", "chineset", "chineses"};
 };
 
 
