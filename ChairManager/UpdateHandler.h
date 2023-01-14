36414744ae789f19285d93c1173b11c237150116
thelivingdiamond <thelivingdiamond6@gmail.com>
2023-01-14 02:52:32 -0800

    Improve Documentation

--- ChairManager/UpdateHandler.h
+++ ChairManager/UpdateHandler.h
@@ -8,20 +8,51 @@
 #include "BinaryVersionCheck.h"
 
 namespace UpdateHandler {
+//! Check if an update is available
+//! \return true if an update is available
     bool isUpdateAvailable();
+
+//! Download the update
     void downloadUpdate();
+
+//! Download the update asynchronously
     void asyncDownloadUpdate();
+
+//! Checks if the async download is still running
     void asyncDownloadCheck();
+
+//! Install the update
     void installUpdate();
+
+//! Install the update asynchronously
     void asyncInstallUpdate();
+
+//! Checks if the async install is still running
     void asyncInstallCheck();
+
+//! Get the progress of the download
     double getProgress();
+
+//! Get the total size of the download
     double getProgressTotal();
+
+//! Is the update handler downloading
+//! \return true if the update handler is downloading
     bool isDownloading();
+
+//! Is the update handler installing
+//! \return true if the update handler is installing
     bool isInstalling();
+
+//! Get the error message
+//! \return the error message
     std::string getError();
+
+//! Is the update handler errored
+//! \return true if the update handler has an error
     bool isErrored();
 
+//! Finishes the update and restarts ChairManager
     void finishUpdate();
 };
 
