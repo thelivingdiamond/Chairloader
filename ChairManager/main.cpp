61b44d456423cc49bae2457a9cf2832c791c87b8
tmp64 <tmp32x2@gmail.com>
2023-04-06 21:17:41 +0700

    ChairManager: Add call stack to unhandled exception handler

--- ChairManager/main.cpp
+++ ChairManager/main.cpp
@@ -4,7 +4,63 @@
 #include <d3d11.h>
 #include <windows.h>
 #include <cstdio>
+#include <StackWalker.h>
 #include "UI.h"
+
+#define EXCEPTION_LOG_FILE_NAME "ChairManagerError.log"
+
+class ChairStackWalker : public StackWalker
+{
+public:
+	using StackWalker::StackWalker;
+
+	const std::string& GetTextBuf() { return m_TextBuf; }
+
+protected:
+	virtual void OnOutput(LPCSTR szText)
+	{
+		m_TextBuf += szText;
+		StackWalker::OnOutput(szText);
+	}
+
+	virtual void OnLoadModule(LPCSTR img,
+		LPCSTR mod,
+		DWORD64 baseAddr,
+		DWORD size,
+		DWORD result,
+		LPCSTR symType,
+		LPCSTR pdbName,
+		ULONGLONG fileVersion) override
+	{
+		// Don't print
+	}
+
+	virtual void OnSymInit(LPCSTR szSearchPath, DWORD symOptions, LPCSTR szUserName) override
+	{
+		// Don't print
+	}
+
+private:
+	std::string m_TextBuf;
+};
+
+void ShowFatalErrorAndDie(ChairStackWalker& sw, const std::string& text)
+{
+	std::string messageText;
+	messageText += "An unhandled exception has occured on the main thread.\n";
+	messageText += "This message is duplicated in " EXCEPTION_LOG_FILE_NAME ".\n\n";
+	messageText += "Exception message:\n";
+	messageText += text + "\n\n";
+	messageText += "Call stack:\n";
+	messageText += sw.GetTextBuf();
+	MessageBoxA(nullptr, messageText.c_str(), "Fatal Error", MB_OK | MB_ICONERROR);
+
+	std::ofstream logFile(EXCEPTION_LOG_FILE_NAME);
+	logFile << messageText;
+	logFile.close(); // Must close because std::exit doesn't return
+
+	std::exit(-1);
+}
 
 int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow) {
 //    AllocConsole();
@@ -16,6 +72,14 @@ int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCm
 	}
 	catch (const std::exception& e)
 	{
-		MessageBoxA(nullptr, e.what(), "Fatal Error", MB_OK | MB_ICONERROR);
+		ChairStackWalker sw;
+		sw.ShowCallstack(GetCurrentThread(), sw.GetCurrentExceptionContext());
+		ShowFatalErrorAndDie(sw, e.what());
+	}
+	catch (...)
+	{
+		ChairStackWalker sw(StackWalker::AfterCatch);
+		sw.ShowCallstack();
+		ShowFatalErrorAndDie(sw, "Unknown Exception Type");
 	}
 }
