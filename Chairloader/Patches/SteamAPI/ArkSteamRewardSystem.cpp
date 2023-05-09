f75f527cbdd6adfa1ef25acdc179b7704c6748a7
tmp64 <tmp32x2@gmail.com>
2023-05-09 08:57:00 +0700

    Patches: ArkSteamRewardSystem: Separate non-Steam code

--- Chairloader/Patches/SteamAPI/ArkSteamRewardSystem.cpp
+++ Chairloader/Patches/SteamAPI/ArkSteamRewardSystem.cpp
@@ -1,6 +1,6 @@
 #include <Chairloader/SteamAPI/IChairSteamAPI.h>
 #include <Chairloader/IChairXmlUtils.h>
-#include "ArkSteamRewardSystem.h"
+#include "SteamAPI/ArkSteamRewardSystem.h"
 
 ArkSteamRewardSystem::ArkSteamRewardSystem()
 {
@@ -35,79 +35,6 @@ bool ArkSteamRewardSystem::LoadRewardData(const string& _strRewardFile)
     return true;
 }
 
-unsigned ArkSteamRewardSystem::IncrementTrackableValue(unsigned _trackable)
-{
-    int oldVal = m_trackables[_trackable].value;
-    int newVal = oldVal + 1;
-    m_trackables[_trackable].value = newVal;
-    TrackableChanged(_trackable, oldVal, newVal);
-    return newVal;
-}
-
-unsigned ArkSteamRewardSystem::IncrementTrackableValue(uint64_t _associatedMetric)
-{
-    if (_associatedMetric == kNoAssociatedMetric)
-        return 0;
-
-    ArkRewardTrackable* pTrackable = m_trackablesByMetric[_associatedMetric];
-
-    if (!pTrackable)
-        return 0;
-
-    // ???
-    if (pTrackable->value >= 0x0FFF'FFFF)
-        return 0;
-
-    return IncrementTrackableValue(pTrackable->id);
-}
-
-unsigned ArkSteamRewardSystem::UpdateTrackableValueMax(uint64_t _associatedMetric, unsigned _value)
-{
-    if (_associatedMetric == kNoAssociatedMetric)
-        return 0;
-
-    ArkRewardTrackable* pTrackable = m_trackablesByMetric[_associatedMetric];
-
-    if (!pTrackable)
-        return 0;
-
-    int oldVal = pTrackable->value;
-    if (_value > oldVal)
-    {
-        pTrackable->value = _value;
-        TrackableChanged(pTrackable->id, oldVal, _value);
-        return _value;
-    }
-    else
-    {
-        return oldVal;
-    }
-}
-
-void ArkSteamRewardSystem::UnlockReward(unsigned _reward)
-{
-    if (IsSuppressed())
-    {
-        CryLog("[ArkSteamRewardSystem] UnlockReward suppressed (rewardId: {})", _reward);
-        return;
-    }
-
-    CryLog("ArkSteamRewardSystem::UnlockReward: {}", _reward);
-
-    ArkReward& reward = m_rewards[_reward];
-
-    if (!reward.bCriteriaMet)
-    {
-        reward.bCriteriaMet = true;
-
-        // Original code passes this to the reward thread, which will call the handler.
-        // I don't want to deal with threading. It shouldn't block anyway.
-        InternalRewardUnlocked(reward.unlockId);
-        if (m_pHandler)
-            m_pHandler->OnRewardUnlocked(m_userId, reward.unlockId);
-    }
-}
-
 void ArkSteamRewardSystem::InternalRewardUnlocked(unsigned unlockId)
 {
     // unlockId = ArkReward::unlockId
@@ -124,46 +51,4 @@ void ArkSteamRewardSystem::InternalRewardUnlocked(unsigned unlockId)
     CryLog("[ArkSteamRewardSystem] Unlocking '{}' (id = {}, name = {})", displayName, unlockId, name);
     SteamUserStats()->SetAchievement(name);
     SteamUserStats()->StoreStats();
-}
-
-void ArkSteamRewardSystem::TrackableChanged(unsigned trackable, unsigned oldVal, unsigned newVal)
-{
-    if (IsSuppressed())
-    {
-        CryLog("[ArkSteamRewardSystem] TrackChanged reward suppressed (trackableId: {}) (oldValue: {}) (newValue: {})",
-            trackable, oldVal, newVal);
-        return;
-    }
-
-    ArkRewardTrackable* pTrackable = &m_trackables[trackable];
-    CryLog("ArkSteamRewardSystem::TrackableChanged: [{}] {} -> {}", trackable, oldVal, newVal);
-
-    // See if need to unlock any rewards
-    for (ArkReward& reward : m_rewards)
-    {
-        bool needUnlock = false;
-
-        for (ArkRewardTrackableCriteria& criteria : reward.criteria)
-        {
-            if (!criteria.bCriteriaMet &&
-                criteria.trackable == pTrackable &&
-                criteria.trackable->value >= criteria.value)
-            {
-                criteria.bCriteriaMet = true;
-                needUnlock = true;
-            }
-        }
-
-        if (needUnlock)
-        {
-            reward.bCriteriaMet = true;
-
-            // Original code passes this to the reward thread 
-            InternalRewardUnlocked(reward.unlockId);
-        }
-    }
-
-    InternalTrackableUpdated(trackable, oldVal, newVal);
-    if (m_pHandler)
-        m_pHandler->OnChangedTrackable(m_userId, trackable, oldVal, newVal);
 }
--- Chairloader/Patches/SteamAPI/ArkSteamRewardSystem.h
+++ Chairloader/Patches/SteamAPI/ArkSteamRewardSystem.h
@@ -1,22 +1,12 @@
 #pragma once
-#include <Prey/CrySystem/Ark/ArkRewardSystem.h>
+#include "ArkFixedRewardSystem.h"
 
 //! Implementation of IArkRewardSystem using Steam API.
-//! Unlike the original version, doesn't user RewardsThread.
-class ArkSteamRewardSystem : public CArkRewardSystem
+class ArkSteamRewardSystem : public ArkFixedRewardSystem
 {
 public:
     ArkSteamRewardSystem();
 
     virtual bool LoadRewardData(const string& _strRewardFile) override;
-    virtual unsigned IncrementTrackableValue(unsigned _trackable) override;
-    virtual unsigned IncrementTrackableValue(uint64_t _associatedMetric) override;
-    virtual unsigned UpdateTrackableValueMax(uint64_t _associatedMetric, unsigned _value) override;
-    virtual void UnlockReward(unsigned _reward) override;
     virtual void InternalRewardUnlocked(unsigned _rewardId) override;
-
-private:
-    //! Original code calls somewhere into gEnv->pSystem->GetIPlatformOS()
-    inline bool IsSuppressed() { return false; }
-    void TrackableChanged(unsigned trackable, unsigned oldVal, unsigned newVal);
 };
