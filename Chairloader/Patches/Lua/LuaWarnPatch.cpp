796f4d6db0153aeb4ab30dcf07e38971d7fd193e
tmp64 <tmp32x2@gmail.com>
2023-06-07 22:02:55 +0700

    Patches: LuaWarnPatch: Handle lua_storedebug

--- Chairloader/Patches/Lua/LuaWarnPatch.cpp
+++ Chairloader/Patches/Lua/LuaWarnPatch.cpp
@@ -1,3 +1,4 @@
+#include <Prey/CrySystem/ICmdLine.h>
 #include <Prey/CryScriptSystem/ScriptSystem.h>
 #include <Prey/CryScriptSystem/ScriptTable.h>
 #include "Lua/LuaWarnPatch.h"
@@ -24,18 +25,41 @@ PreyFunction<void(lua_State* L, int idx)> lua_insert(0xD2BF80);
 PreyFunction<int(lua_State* L, int idx)> lua_setfenv(0xD2C980);
 PreyFunction<int(lua_State* L, int nargs, int nresults, int errfunc)> lua_pcall(0xD2C280);
 PreyFunction<void(lua_State* L, int idx)> lua_remove(0xD2C860);
+PreyFunction<void(lua_State* L, int e)> lua_storedebuginfo(0xD41210);
+PreyFunction<int(lua_State* L)> lua_isstoredebuginfo(0xD41050);
 
 auto g_ReloadScriptCmd_Hook = g_ReloadScriptCmd.MakeHook();
 auto g_CScriptSystem_ErrorHandler_Hook = CScriptSystem::FErrorHandler.MakeHook();
 auto g_CScriptSystem_ExecuteBuffer_Hook = CScriptSystem::FExecuteBuffer.MakeHook();
 auto g_CScriptSystem_RaiseError_Hook = CScriptSystem::FRaiseError.MakeHook();
+auto g_lua_storedebuginfo_Hook = lua_storedebuginfo.MakeHook();
 
 void PushTable(IScriptTable* pTable)
 {
 	((CScriptTable*)pTable)->PushRef();
 };
 
-void ReloadScriptCmd(IConsoleCmdArgs* pArgs)
+void lua_storedebuginfo_Hook(lua_State* L, int e)
+{
+	static bool isSet = false;
+
+	if (!isSet)
+	{
+		isSet = true;
+
+		// On first call (during CScriptSystem::Init) check -lua_storedebug cmd line arg
+		const ICmdLineArg* pArg = gEnv->pSystem->GetICmdLine()->FindArg(eCLAT_Pre, "lua_storedebug");
+		if (pArg && pArg->GetIValue())
+		{
+			g_lua_storedebuginfo_Hook.InvokeOrig(L, 1);
+			return;
+		}
+	}
+
+	g_lua_storedebuginfo_Hook.InvokeOrig(L, e);
+}
+
+void ReloadScriptCmd_Hook(IConsoleCmdArgs* pArgs)
 {
     if (pArgs->GetArgCount() < 2)
         g_pLuaLog->LogWarning("No file specified for reload");
@@ -45,6 +69,10 @@ void ReloadScriptCmd(IConsoleCmdArgs* pArgs)
 
 int CScriptSystem_ErrorHandler_Hook(lua_State* L)
 {
+	// Ignore errors if debugging is not enabled
+	if (!lua_isstoredebuginfo(L))
+		return 0;
+
     const char* sErr = lua_tostring(L, 1);
     if (sErr)
         g_pLuaLog->LogError("{}", sErr);
@@ -108,7 +136,7 @@ void CScriptSystem_RaiseError_Hook(CScriptSystem* const _this, const char* forma
 	cry_vsprintf(sBuf, format, arglist);
 	va_end(arglist);
 
-	g_pLuaLog->LogError("%s", sBuf);
+	g_pLuaLog->LogError("{}", sBuf);
 
 	_this->TraceScriptError(sSourceFile, nCurrentLine, sBuf);
 }
@@ -120,7 +148,8 @@ void LuaWarnPatch::InitSystem()
     g_pLuaLog = gCL->cl->CreateLogger();
     g_pLuaLog->SetName("Lua");
 
-    g_ReloadScriptCmd_Hook.SetHookFunc(&ReloadScriptCmd);
+	g_lua_storedebuginfo_Hook.SetHookFunc(&lua_storedebuginfo_Hook);
+    g_ReloadScriptCmd_Hook.SetHookFunc(&ReloadScriptCmd_Hook);
     g_CScriptSystem_ErrorHandler_Hook.SetHookFunc(&CScriptSystem_ErrorHandler_Hook);
 	g_CScriptSystem_ExecuteBuffer_Hook.SetHookFunc(&CScriptSystem_ExecuteBuffer_Hook);
 	g_CScriptSystem_RaiseError_Hook.SetHookFunc((void(*)(CScriptSystem* const _this, const char* format))&CScriptSystem_RaiseError_Hook); // Uses variadic args - have to hack around it
