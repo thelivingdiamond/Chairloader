3ce51664c9032bbafd9821ae786bed3b35dd3886
tmp64 <tmp32x2@gmail.com>
2023-09-01 10:17:20 +0700

    CI: Debug space issues

---
+++ Scripts/ci_space_debug.py
@@ -0,0 +1,29 @@
+import shutil
+import string
+from ctypes import windll
+
+
+def sizeof_fmt(num, suffix="B"):
+    for unit in ("", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"):
+        if abs(num) < 1024.0:
+            return f"{num:3.1f}{unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f}Yi{suffix}"
+
+
+def get_drives():
+    drives = []
+    bitmask = windll.kernel32.GetLogicalDrives()
+    for letter in string.ascii_uppercase:
+        if bitmask & 1:
+            drives.append(letter)
+        bitmask >>= 1
+
+    return drives
+
+
+drives = get_drives()
+for i in drives:
+    usage = shutil.disk_usage(f'{i}:')
+    print(f'{i}: total = {sizeof_fmt(usage.total)}; used = {sizeof_fmt(usage.used)}; free = {sizeof_fmt(usage.free)}')
+
