thelivingdiamond/Chairloader

commit 46bc225186f498e945fc67b4e121cf80bc596897

XMLMerger2: Fix attribute matching when attribute is missing

CommonChairManager/Manager/XMLMerger2.cpp
@@ -409,8 +409,10 @@ pugi::xml_node XMLMerger2::FindNodeByAttributeList(pugi::xml_node &searchNode, p
     if(!matchAllAttributes) {
         for(auto &child: searchNode){
             for (auto &attribute: attributeList) {
-                std::string attributeValue = referenceNode.attribute(attribute.attribute_name.c_str()).value();
-                if (child.attribute(attribute.attribute_name.c_str()).value() == attributeValue) {
+                pugi::xml_attribute referenceAttr = referenceNode.attribute(attribute.attribute_name.c_str());
+                pugi::xml_attribute childAttr = child.attribute(attribute.attribute_name.c_str());
+
+                if (referenceAttr && childAttr && !strcmp(referenceAttr.value(), childAttr.value())) {
                     searchResult = child;
                     break;
                 }
@@ -422,8 +424,10 @@ pugi::xml_node XMLMerger2::FindNodeByAttributeList(pugi::xml_node &searchNode, p
         for(auto &child: searchNode){
             bool found = true;
             for (auto &attribute: attributeList) {
-                std::string attributeValue = referenceNode.attribute(attribute.attribute_name.c_str()).value();
-                if (child.attribute(attribute.attribute_name.c_str()).value() != attributeValue) {
+                pugi::xml_attribute referenceAttr = referenceNode.attribute(attribute.attribute_name.c_str());
+                pugi::xml_attribute childAttr = child.attribute(attribute.attribute_name.c_str());
+
+                if (referenceAttr && childAttr && !strcmp(referenceAttr.value(), childAttr.value())) {
                     found = false;
                     break;
                 }

commit 7cc3a2ad684e2a7094ef9f2d9cdff0e071e9a683

XMLMerger2: Fix FindNodeByAttributeList
Broken by 46bc225186f498e945fc67b4e121cf80bc596897

CommonChairManager/Manager/XMLMerger2.cpp
@@ -427,7 +427,7 @@ pugi::xml_node XMLMerger2::FindNodeByAttributeList(pugi::xml_node &searchNode, p
                 pugi::xml_attribute referenceAttr = referenceNode.attribute(attribute.attribute_name.c_str());
                 pugi::xml_attribute childAttr = child.attribute(attribute.attribute_name.c_str());
 
-                if (referenceAttr && childAttr && !strcmp(referenceAttr.value(), childAttr.value())) {
+                if (!referenceAttr || !childAttr || strcmp(referenceAttr.value(), childAttr.value())) {
                     found = false;
                     break;
                 }