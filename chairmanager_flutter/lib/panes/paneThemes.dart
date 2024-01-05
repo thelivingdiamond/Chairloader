a75a0f027a62cd30880706038a2b9c7b32936738
thelivingdiamond <thelivingdiamond6@gmail.com>
2024-01-04 20:50:26 -0800

    Update pane themes

--- chairmanager_flutter/lib/panes/paneThemes.dart
+++ chairmanager_flutter/lib/panes/paneThemes.dart
@@ -2,7 +2,7 @@ import 'package:fluent_ui/fluent_ui.dart';
 
 const Color tileColor = Color.fromARGB(255, 35, 35, 35);
 // Color get titleColor => tileColor;
-const Color titleColor = Color.fromARGB(255, 42, 42, 42);
+const Color titleColor = Color.fromARGB(255, 45, 45, 45);
 
 
 const TextStyle titleStyle = TextStyle(
