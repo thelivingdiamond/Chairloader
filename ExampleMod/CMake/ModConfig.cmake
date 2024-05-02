eded386b261d7cc492e826dacb102ac08bc2b3f5
tmp64 <tmp32x2@gmail.com>
2024-05-02 22:05:04 +0700

    ExampleMod: Fix output path in DebugChair config

--- ExampleMod/CMake/ModConfig.cmake
+++ ExampleMod/CMake/ModConfig.cmake
@@ -15,6 +15,7 @@ function(mod_output_dir mod_target mod_prefix)
 		set_target_properties(${mod_target} PROPERTIES
 			RUNTIME_OUTPUT_DIRECTORY ${mod_dll_path}
 			RUNTIME_OUTPUT_DIRECTORY_DEBUG ${mod_dll_path}
+			RUNTIME_OUTPUT_DIRECTORY_DEBUGCHAIR ${mod_dll_path}
 			RUNTIME_OUTPUT_DIRECTORY_RELEASE ${mod_dll_path}
 			RUNTIME_OUTPUT_DIRECTORY_RELWITHDEBINFO ${mod_dll_path}
 			RUNTIME_OUTPUT_DIRECTORY_MINSIZEREL ${mod_dll_path}
