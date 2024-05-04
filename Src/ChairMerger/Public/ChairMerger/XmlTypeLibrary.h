b8281d257b18d67365ef3897011c8d7b7e1e5f3c
tmp64 <tmp32x2@gmail.com>
2024-05-04 20:25:10 +0700

    ChairMerger: Add XML Type Library

---
+++ Src/ChairMerger/Public/ChairMerger/XmlTypeLibrary.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <ChairMerger/Export.h>
+
+struct CHAIRMERGER_EXPORT IXmlType
+{
+    virtual ~IXmlType() {}
+
+    //! @returns The type name.
+    virtual const std::string& GetName() const = 0;
+
+    //! @returns The type name with additional comments.
+    virtual const std::string& GetFullName() const = 0;
+
+    //! Validates a value.
+    virtual bool ValidateValue(std::string_view value) const = 0;
+};
+
+class CHAIRMERGER_EXPORT XmlTypeLibrary : NoCopy
+{
+public:
+    XmlTypeLibrary();
+    ~XmlTypeLibrary();
+
+    //! Finds a type by its name.
+    //! @returns Type or nullptr if not found.
+    const IXmlType* FindType(std::string_view typeName) const;
+
+private:
+    std::map<std::string, std::unique_ptr<IXmlType>, std::less<>> m_Types;
+
+    //! Registers a new type.
+    void RegisterType(std::unique_ptr<IXmlType>&& ptr);
+
+    //! Registers a type alias. Acts like a separate type but actually calls an existing one.
+    void RegisterAlias(std::string_view newName, std::string_view existingName);
+};
