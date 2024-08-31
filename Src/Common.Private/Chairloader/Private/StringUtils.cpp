3045b29f764118523cc846d16082b412a968bf93
tmp64 <tmp32x2@gmail.com>
2024-08-31 09:19:54 +0700

    Common.Private: StringUtils: Fix TrimStringView

--- Src/Common.Private/Chairloader/Private/StringUtils.cpp
+++ Src/Common.Private/Chairloader/Private/StringUtils.cpp
@@ -3,12 +3,12 @@
 std::string_view TrimStringView(std::string_view s, const char* spaces)
 {
     size_t begin = s.find_first_not_of(spaces);
-    size_t end = s.find_last_not_of(spaces);
+    size_t end = s.find_last_not_of(spaces) + 1;
     begin = std::min(begin, s.size());
-    end = std::min(end, s.size());
+    end = std::min(end, s.size() + 1);
 
     if (begin == end)
         return std::string_view();
     else
-        return s.substr(begin, end - begin + 1);
+        return s.substr(begin, end - begin);
 }
