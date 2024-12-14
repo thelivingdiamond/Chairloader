8fce96febfdc4b614037b2dd19f3ed7eb7510a42
thelivingdiamond <thelivingdiamond6@gmail.com>
2024-12-14 12:14:35 -0800

    Critical feature added

--- chairmanager_flutter_v2/lib/pages/splash/SplashScreen.dart
+++ chairmanager_flutter_v2/lib/pages/splash/SplashScreen.dart
@@ -58,18 +58,18 @@ class SplashScreen extends StatelessWidget {
                     children: [
                       if(!kDebugMode || forceAnimation)
                         Container(
-                          child:   Particles(
-                            awayRadius: 150,
-                            particles: createParticles(150, 25.0, 10.0, Theme.of(context).colorScheme.primaryContainer.withOpacity(0.5)),
-                            height: MediaQuery.of(context).size.height,
-                            width: MediaQuery.of(context).size.width,
-                            onTapAnimation: true,
-                            awayAnimationDuration: const Duration(milliseconds: 100),
-                            awayAnimationCurve: Curves.linear,
-                            enableHover: false,
-                            hoverRadius: 5,
-                            connectDots: true,
-                          )
+                            child:   Particles(
+                              awayRadius: 150,
+                              particles: createParticles(150, 25.0, 10.0, Theme.of(context).colorScheme.primaryContainer.withOpacity(0.5)),
+                              height: MediaQuery.of(context).size.height,
+                              width: MediaQuery.of(context).size.width,
+                              onTapAnimation: true,
+                              awayAnimationDuration: const Duration(milliseconds: 100),
+                              awayAnimationCurve: Curves.linear,
+                              enableHover: false,
+                              hoverRadius: 5,
+                              connectDots: true,
+                            )
                         ),
                       if(kDebugMode && !forceAnimation)
                         Center(
@@ -83,27 +83,139 @@ class SplashScreen extends StatelessWidget {
                           ),
                         )
                       else
-                      Center(
-                        child: Transform.scale(
-                          scale: 0.5,
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Theme.of(context).colorScheme.primaryContainer,
-                              borderRadius: BorderRadius.circular(40),
+                        if(!controller.gachaBegin)
+                          Center(
+                            child: GestureDetector(
+                              onDoubleTap: () => controller.triggerGacha(context),
+                              child: Transform.scale(
+                                  scale: 0.5,
+                                  child: Animate(
+                                    child: const Image(image: AssetImage('assets/ChairManager_Icon.png'))
+                                        .animate(target: controller.gachaMode ? 1 : 0,)
+                                        .custom(builder: (context, value, child) => Container(
+                                      decoration: BoxDecoration(
+                                        color: Color.lerp(Theme.of(context).colorScheme.primaryContainer, Theme.of(context).colorScheme.secondaryFixed, value),
+                                        borderRadius: BorderRadius.circular(40),
+                                      ),
+                                      child: child,
+                                    ),)
+                                        .animate(
+                                      onComplete: (animationController) async => await controller.endOfSplashScreen(context),)
+                                        .slideY(begin: 1.5, end: 0, duration: 2000.ms, curve: Curves.easeInOutCubic)
+                                        .blur(duration: 2000.ms, curve: Curves.easeInOutCubic, begin: const Offset(20, 20), end: Offset.zero)
+                                        .fadeIn(duration: 2000.ms, curve: Curves.easeInOutCubic)
+                                        .then()
+                                        .shake(duration: 900.ms, curve: Curves.easeInOutCubic, hz: 5)
+                                        .scale(begin: const Offset(1, 1), end: const Offset(2, 2), duration: 1000.ms, curve: Curves.elasticIn)
+                                        .fadeOut(delay: 800.ms, duration: 150.ms, curve: Curves.easeInOut)
+                                        .then(),
+                                  )
+                              ),
                             ),
-                              child: const Image(image: AssetImage('assets/ChairManager_Icon.png'))
                           )
-                              .animate(onComplete: (animationController) => context.go("/"),)
-                              .slideY(begin: 1.5, end: 0, duration: 2000.ms, curve: Curves.easeInOutCubic)
-                              .blur(duration: 2000.ms, curve: Curves.easeInOutCubic, begin: const Offset(20, 20), end: Offset.zero)
-                              .fadeIn(duration: 2000.ms, curve: Curves.easeInOutCubic)
-                              .then()
-                              .shake(duration: 900.ms, curve: Curves.easeInOutCubic, hz: 5)
-                              .scale(begin: const Offset(1, 1), end: const Offset(2, 2), duration: 1000.ms, curve: Curves.elasticIn)
-                              .fadeOut(delay: 800.ms, duration: 150.ms, curve: Curves.easeInOut)
-                              .then(),
-                        ),
-                      ),
+                        else
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                height: 400,
+                                width: 300,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      color: Colors.black26.withOpacity(0.5),
+                                      border: Border.all(
+                                        color: controller.getGachaColor(),
+                                        width: 5,
+                                      ),
+                                      borderRadius: BorderRadius.circular(20)
+                                  ),
+                                  child: Center(
+                                    //TODO: get actual chairacter images (not a typo)
+                                      child: Text(controller.getGachaText(), style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold, color: controller.getGachaColor()), textAlign: TextAlign.center,)
+                                  ),
+                                )
+                                    .animate(onComplete: (animationController) async => await controller.finishGacha(),)
+                                    .then(delay: ((controller.gachaRarity + 1) * 1000).ms)
+                                    .boxShadow(
+                                      duration: 600.ms,
+                                      curve: Curves.easeOutCubic,
+                                      begin: BoxShadow(
+                                          color: controller.getGachaColor().withOpacity(0.0),
+                                          blurRadius: 40,
+                                          spreadRadius: -5,
+                                          blurStyle: BlurStyle.outer
+                                      ),
+                                      end:  BoxShadow(
+                                          color: controller.getGachaColor().withOpacity(controller.gachaRarity * 0.1),
+                                          blurRadius: controller.gachaRarity * 20 + 40,
+                                          spreadRadius: -5
+                                      ),
+                                    )
+                                    .slideY(duration: 600.ms)
+                                    .fadeIn(duration: 600.ms)
+                                    .scale(begin: const Offset(0.5, 0.5), end: const Offset(1, 1), duration: 600.ms)
+                                    .then(),
+                              ),
+                              Container(
+                                  child: Center(
+                                    child: Stack(
+                                      children: [
+                                        Row(
+                                          mainAxisAlignment: MainAxisAlignment.center,
+                                          children: [
+                                            for(int i = 0; i < 5; i++)
+                                              Container(
+                                                padding: const EdgeInsets.all(8.0),
+                                                child: ColorFiltered(
+                                                    colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.9), BlendMode.srcATop),
+                                                    child: const Image(image: AssetImage('assets/ChairManager_Icon.png'), width: 100, height: 100, )
+                                                ),
+                                              ),
+                                          ],
+                                        ),
+                                        Row(
+                                          mainAxisAlignment: MainAxisAlignment.center,
+                                          children: [
+                                            for(int i = 0; i < 5; i++)
+                                              Container(
+                                                padding: const EdgeInsets.all(8.0),
+                                                child: const Image(image: AssetImage('assets/ChairManager_Icon.png'), width: 100, height: 100,),
+                                              )
+                                                  .animate(target: i <= controller.gachaRarity ? 1 : 0,)
+                                                  .then(delay: (i * 1000).ms)
+                                                  .scale(begin: const Offset(1.5, 1.5), end: const Offset(1, 1), duration: 1000.ms, curve: Curves.easeOutCubic)
+                                                  .rotate(begin: 0, end: 8, duration: 1000.ms, curve: Curves.easeOutCubic)
+                                                  .fadeIn(duration: 1000.ms, curve: Curves.ease)
+                                                  .shake(duration: 900.ms, curve: Curves.easeInOutCubic, hz: 5)
+                                                  .tint(delay: 900.ms, duration: 100.ms, curve: Curves.linear, color: controller.getGachaColor())
+                                                  .then()
+                                                  .shake(duration: 100.ms)
+                                                  .boxShadow(
+                                                    duration: 300.ms,
+                                                    curve: Curves.easeOutCubic,
+                                                    begin: BoxShadow(
+                                                        color: controller.getGachaColor().withOpacity(0.0),
+                                                        blurRadius: 40,
+                                                        spreadRadius: -40,
+                                                    ),
+                                                    end:  BoxShadow(
+                                                        color: controller.getGachaColor().withOpacity(controller.gachaRarity * 0.1),
+                                                        blurRadius: 40,
+                                                        spreadRadius: -20
+                                                    ),
+                                                  ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                              ),
+                              FilledButton(onPressed: () => context.go("/"), child: const Text("Continue"))
+                                  .animate(target: controller.gachaFinished ? 1 : 0,)
+                                  .fadeIn(duration: 1000.ms)
+                                  .then(),
+                            ],
+                          ),
                     ],
                   )
               ),
--- chairmanager_flutter_v2/lib/pages/splash/SplashScreenController.dart
+++ chairmanager_flutter_v2/lib/pages/splash/SplashScreenController.dart
@@ -1,5 +1,92 @@
+import 'dart:math';
+
+import 'package:chairmanager_flutter_v2/dialogs/deploy/DeployDialog.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
 
 class SplashScreenController extends GetxController {
   SplashScreenController();
+
+  bool gachaMode = false;
+  bool gachaBegin = false;
+  int gachaRarity = 0;
+  bool gachaFinished = false;
+
+  final commonRarity = 40;
+  final uncommonRarity = 30;
+  final rareRarity = 20;
+  final epicRarity = 8;
+  final legendaryRarity = 2;
+
+  Future<void> triggerGacha(BuildContext context) async {
+    gachaMode = true;
+    update();
+  }
+
+  Future<void> endOfSplashScreen(BuildContext context) async {
+    if(gachaMode) {
+      await beginGacha();
+    } else {
+      context.go("/");
+    }
+  }
+
+  Future<void> beginGacha() async {
+    gachaBegin = true;
+
+    final random = Random().nextInt(101);
+    if(random < commonRarity) {
+      gachaRarity = 0;
+    } else if(random < commonRarity + uncommonRarity) {
+      gachaRarity = 1;
+    } else if(random < commonRarity + uncommonRarity + rareRarity) {
+      gachaRarity = 2;
+    } else if(random < commonRarity + uncommonRarity + rareRarity + epicRarity) {
+      gachaRarity = 3;
+    } else {
+      gachaRarity = 4;
+    }
+    update();
+  }
+
+  Future<void> finishGacha() async {
+    gachaFinished = true;
+    update();
+  }
+
+  Color getGachaColor() {
+    switch(gachaRarity) {
+      case 0:
+        return Colors.grey;
+      case 1:
+        return Colors.lightGreen;
+      case 2:
+        return Colors.lightBlueAccent;
+      case 3:
+        return Colors.deepPurple;
+      case 4:
+        return Colors.orange;
+      default:
+        return Color(0xFF000000);
+    }
+  }
+
+  String getGachaText() {
+    switch(gachaRarity) {
+      case 0:
+        return "Common\nChair";
+      case 1:
+        return "Uncommon\nChair";
+      case 2:
+        return "Rare\nChair";
+      case 3:
+        return "Epic\nChair";
+      case 4:
+        return "Legendary\nChair";
+      default:
+        return "Error\nChair";
+    }
+  }
 }
